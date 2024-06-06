61b7ea64cc46045fde70a7dc8d8098f27b23bd89
vova <volodymyrtymets@gmail.com>
2024-06-06 12:51:56 +0300

    remove redis from low-freq-generator

--- low-freq-generator/index.js
+++ low-freq-generator/index.js
@@ -1,67 +1,66 @@
-const redis = require('redis');
 const Gpio = require('onoff').Gpio;
 
+let DEFAULT_FREQ = 15;
 let out1 = null;
 let out2 = null;
 let TIMES = [];
+let FREQ = (process.argv[2] && parseInt(process.argv[2]) || parseInt(process.env.FREQ, 10) || DEFAULT_FREQ);
+let TYPE = (process.argv[3] || parseInt(process.env.POSITION, 10) || 1);
 let STEP = 1;
-let REDIS_ARGS = [];
-let DEFAULT_FREQ = 15;
-
-const getRedisParams = (client) =>
-  Promise.all([
-    client.get('settings:frequency'),
-    client.get('settings:frequency-type'),
-    client.get('settings:frequency-corelation')
-  ])
 
 const getProcessParams = (buffer) => {
-  const args = buffer.toString().split('settings:frequency:');
-  const freqArgs = args.length && args[1];
-  const freq = parseInt(freqArgs);
-
-  if (Number.isInteger(freq)) {
-    return [freq, undefined, undefined]
+  const [key, valueStr] = buffer.toString().split(':');
+  const value = parseInt(valueStr);
+  if (!Number.isInteger(value)) {
+    return [FREQ, TYPE]
   }
-  return  [DEFAULT_FREQ, undefined, undefined]
+  if(key === 'f') {
+    return [value, TYPE];
+  }
+  if(key === 't') {
+    return [FREQ, value];
+  }
+  return  [FREQ, TYPE]
 }
 
 
-const setIterationTimes = (freq, position = 1, correlation = 1) => {
-  console.log(`[RG_app]-> Set frequency [${freq}], position [${position}], correlation [${correlation}]`);
+const setIterationTimes = (freq, position) => {
+  console.log(`[RG_app]-> Set frequency [${freq}], position [${position}]`);
   const stepTime = Math.round((1000 / freq) / 6);
   const timeMap = {
-    '1': [stepTime, Math.round(stepTime * correlation), stepTime, stepTime, Math.round(stepTime * correlation), stepTime],
-    '2': [0, Math.round(stepTime * correlation), stepTime, 0, Math.round(stepTime * correlation), stepTime],
+    '1': [stepTime, stepTime , stepTime, stepTime, stepTime, stepTime],
+    '2': [0, stepTime, stepTime, 0, stepTime, stepTime],
   };
   TIMES = timeMap[position] || timeMap['1'];
+  FREQ = freq
+  TYPE = position
 }
 
 const iterator = () => {
   if (STEP === 1) {
-    out1.writeSync(0)
-    out2.writeSync(0)
+    // out1.writeSync(0)
+    // out2.writeSync(0)
   }
   if (STEP === 2) {
-    out1.writeSync(1)
-    out2.writeSync(0)
+    // out1.writeSync(1)
+    // out2.writeSync(0)
   }
   if (STEP === 3) {
-    out1.writeSync(0)
-    out2.writeSync(0)
+    // out1.writeSync(0)
+    // out2.writeSync(0)
   }
   if (STEP === 4) {
-    out1.writeSync(0)
-    out2.writeSync(0)
+    // out1.writeSync(0)
+    // out2.writeSync(0)
   }
   if (STEP === 5) {
-    out1.writeSync(0)
-    out2.writeSync(1)
+    // out1.writeSync(0)
+    // out2.writeSync(1)
   }
   if (STEP === 6) {
     STEP = 1;
-    out2.writeSync(0);
-    out2.writeSync(0);
+    // out2.writeSync(0);
+    // out2.writeSync(0);
   }
   TIMES[STEP] - 1 && setTimeout(iterator, TIMES[STEP - 1]);
   STEP++;
@@ -70,9 +69,6 @@ const iterator = () => {
 const main = () => {
   const outNumber1 = process.argv[5] && parseInt(process.argv[6]) || 14;
   const outNumber2 = process.argv[6] && parseInt(process.argv[6]) || 15;
-  const freqArg = (process.argv[2] && parseInt(process.argv[2]) || parseInt(process.env.FREQ, 10) || DEFAULT_FREQ);
-  const freqPositionArg = process.argv[3] || parseInt(process.env.POSITION, 10) || 1;
-  const freqCorrelationArg = process.argv[4] && parseFloat(process.argv[4]) || parseFloat(process.env.COR) || 1;
 
   try {
     console.log(`[RG_app]-> Start on GPIO [${outNumber1}] [${outNumber2}]`);
@@ -87,29 +83,11 @@ const main = () => {
     } catch (err) {
       console.log('[RG_app]-> Error: GPIO is not detected!!!');
       console.error(err)
-      process.exit();
+      // process.exit();
     }
   }
-  setIterationTimes(freqArg, freqPositionArg, freqCorrelationArg);
+  setIterationTimes(FREQ, TYPE);
   iterator();
-
-  redis.createClient()
-    .on('error', err => console.error('[RG_app]-> Redis Client Error', err))
-    .connect().then(client => {
-    console.log('[RG_app]-> Redis successfully connected:');
-    getRedisParams(client).then((res) => {
-      REDIS_ARGS = res;
-    })
-
-    setInterval(() => {
-      getRedisParams(client).then((res) => {
-        if (REDIS_ARGS[0] !== res[0] || REDIS_ARGS[1] !== res[1] || REDIS_ARGS[2] !== res[2]) {
-          setIterationTimes(res[0], res[1], res[2]);
-          REDIS_ARGS = res;
-        }
-      })
-    }, 700)
-  })
 }
 
 // on parent process message
